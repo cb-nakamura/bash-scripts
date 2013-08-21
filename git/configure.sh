d0c23fbf83837b1070eb57fda0d13c9b4f6a7fd5
Tadashi Nakamura <tds.nakamura@gmail.com>
2013-08-21 17:32:34 +0900

    script to configure git environment.

---
+++ git/configure.sh
@@ -0,0 +1,15 @@
+# 名前
+git config --global user.name "Tadashi Nakamura"
+# メールアドレス
+git config --global user.email tds.nakamura@gmail.com
+# 色設定
+git config --global color.ui=auto
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+
+# 設定内容確認
+git config --list
+
