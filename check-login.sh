50c7056a935fe298676555b11e624d060cb49de4
Tadashi Nakamura <tds.nakamura@gmail.com>
2013-08-26 18:00:06 +0900

    Create check-login.sh

---
+++ check-login.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SERVERS="www0
+www1
+www2";
+
+for SERVER in $SERVERS; do
+	expect -c "
+	spawn ssh -o \"StrictHostKeyChecking no\" $SERVER 'hostname'
+	expect 'password:'
+	send PASSWORD\r
+	interact
+    "
+done;
